e9ce94434d09d2fcf7a67ce7d8961ea895facfaf
JackPadalino <pada0867@gmail.com>
2022-09-09 22:56:02 -0400

    able to read and convert CSV file

---
+++ readCSV.js
@@ -0,0 +1,30 @@
+const fs=require('fs');
+const {parse}=require('csv-parse');
+
+const filePath='/Users/snerd/desktop/top50.csv'
+
+fs.createReadStream(filePath);
+
+// Read the CSV file and turn it into an array of objects
+const data = [];
+
+fs.createReadStream(filePath)
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  })
+  .on("end", function () {
+    // log the result array
+    console.log("parsed csv data:");
+    console.log(data);
+  });
