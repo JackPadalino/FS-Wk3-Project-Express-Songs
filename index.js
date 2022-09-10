e24d872b68de3064b96e02da0e2dd7f9f63d04ee
JackPadalino <pada0867@gmail.com>
2022-09-10 14:39:45 -0400

    updated HTML

--- index.js
+++ index.js
@@ -18,7 +18,42 @@ app.get('/health',(req,res)=>{
 
 // endpoint for entire array
 app.get('/songs',(req,res)=>{
-    const songData=dummyData.list();
+  const songData=dummyData.list();
+  const html = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Song DB</title>
+    <link rel='stylesheet' href='/style.css'/>
+  </head>
+  <body>
+    <div id="mainContainer">
+      <h1 id="mainTitle">Songs</h1>
+      <div id='songListContainer'>
+        ${songData.map(
+          (song) =>
+            `
+            <div class='songContainer'>
+              <a href="/songs/${song.id}/"><b>${song['Track.Name']}</b> by ${song['Artist.Name']}</a>
+              <p>${song.Genre}</p>
+            </div>
+            `
+        ).join('')}
+      </div>
+    </div>
+  </body>
+  </html>
+  `;
+  res.send(html);
+});
+
+// endpoint for a single song
+app.get('/songs/:songId',(req,res)=>{
+  const songId=req.params.songId;
+  const song = dummyData.find(songId);
+  if (!song.id){
+    throw new Error(`Song with ID:${songId} not found.`)
+  }else{
     const html = `
     <!DOCTYPE html>
     <html>
@@ -27,55 +62,20 @@ app.get('/songs',(req,res)=>{
       <link rel='stylesheet' href='/style.css'/>
     </head>
     <body>
-      <div id="mainContainer">
-        <h1 id="mainTitle">Songs</h1>
-        <div id='songListContainer'>
-          ${songData.map(
-            (song) =>
-              `
-              <div class='songContainer'>
-                <a href="/songs/${song.id}/"><b>${song['Track.Name']}</b> by ${song['Artist.Name']}</a>
-                <p>${song.Genre}</p>
-              </div>
-              `
-          ).join('')}
-        </div>
+      <div class='songDetailsContainer'>
+        <h1 id="songTitle">${song['Track.Name']}</h1>
+        <h3>${song['Artist.Name']}</h3>
+        <ul>
+          <li><b>Genre:</b> ${song.Genre}</li>
+          <li><b>BPM:</b> ${song['Beats.Per.Minute']}</li>
+          <li><b>Energy:</b> ${song.Energy}</li>
+        </ul>
       </div>
     </body>
     </html>
     `;
     res.send(html);
-});
-
-// endpoint for a single song
-app.get('/songs/:songId',(req,res)=>{
-    const songId=req.params.songId;
-    const song = dummyData.find(songId);
-    if (!song.id){
-      throw new Error(`Song with ID:${songId} not found.`)
-    }else{
-      const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Song DB</title>
-        <link rel='stylesheet' href='/style.css'/>
-      </head>
-      <body>
-        <div class='songDetailsContainer'>
-          <h1 id="songTitle">${song['Track.Name']}</h1>
-          <h3>${song['Artist.Name']}</h3>
-          <ul>
-            <li><b>Genre:</b> ${song.Genre}</li>
-            <li><b>BPM:</b> ${song['Beats.Per.Minute']}</li>
-            <li><b>Energy:</b> ${song.Energy}</li>
-          </ul>
-        </div>
-      </body>
-      </html>
-      `;
-      res.send(html);
-    };
+  };
 });
 
 // using the error handler as middleware
